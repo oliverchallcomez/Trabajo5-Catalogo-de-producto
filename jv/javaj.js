6bc717ae779b6cab950ea7e667f3da8801f914e0
oliverchallcomez <oliverchallcomeza@gmail.com>
2023-07-24 22:07:18 -0500

    Subiendo Trabajo5-Catalogo-de-producto

---
+++ jv/javaj.js
@@ -0,0 +1,73 @@
+
+
+document.getElementById("1").innerHTML = descuento(10.00,50.00);
+function descuento (numero, porcentaje){
+    return Math.abs(numero * porcentaje / 100 - numero)
+}
+
+
+document.getElementById("2").innerHTML = descuento2(10.00,50.00);
+function descuento2 (numero, porcentaje){
+    return Math.abs(numero * porcentaje / 100 - numero)
+}
+
+
+document.getElementById("3").innerHTML = descuento3(10.00,50.00);
+function descuento3 (numero, porcentaje){
+    return Math.abs(numero * porcentaje / 100 - numero)
+}
+
+
+document.getElementById("4").innerHTML = descuento4(10.00,50.00);
+function descuento4 (numero, porcentaje){
+    return Math.abs(numero * porcentaje / 100 - numero)
+}
+
+
+document.getElementById("5").innerHTML = descuento5(10.00,50.00);
+function descuento5 (numero, porcentaje){
+    return Math.abs(numero * porcentaje / 100 - numero)
+}
+
+
+document.getElementById("6").innerHTML = descuento6(10.00,50.00);
+function descuento6 (numero, porcentaje){
+    return Math.abs(numero * porcentaje / 100 - numero)
+}
+
+
+document.getElementById("7").innerHTML = descuento7(10.00,50.00);
+function descuento7 (numero, porcentaje){
+    return Math.abs(numero * porcentaje / 100 - numero)
+}
+
+
+document.getElementById("8").innerHTML = descuento8(10.00,50.00);
+function descuento8 (numero, porcentaje){
+    return Math.abs(numero * porcentaje / 100 - numero)
+}
+
+
+document.getElementById("9").innerHTML = descuento9(10.00,50.00);
+function descuento9 (numero, porcentaje){
+    return Math.abs(numero * porcentaje / 100 - numero)
+}
+
+
+document.getElementById("10").innerHTML = descuento10(10.00,50.00);
+function descuento10 (numero, porcentaje){
+    return Math.abs(numero * porcentaje / 100 - numero)
+}
+
+document.getElementById("11").innerHTML = descuento11(10.00,50.00);
+function descuento11 (numero, porcentaje){
+    return Math.abs(numero * porcentaje / 100 - numero)
+}
+
+document.getElementById("12").innerHTML = descuento12(10.00,50.00);
+function descuento12 (numero, porcentaje){
+    return Math.abs(numero * porcentaje / 100 - numero)
+}
+
+
+
